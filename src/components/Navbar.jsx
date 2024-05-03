c226c3fe5f73a7375e18bd9c34831338e2e81f9c
Lucky Agboola <luckyagboola@gmail.com>
2024-05-03 04:33:49 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { DiRedis } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { animate, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl">
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800">
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800">
+          <TbBrandNextjs className="text-7xl" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800">
+          <SiMongodb className="text-7xl text-green-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800">
+          <DiRedis className="text-7xl text-red-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800">
+          <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800">
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
